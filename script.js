cc8562f6d611d3c6d3c0dac4253a337b628129b5
begoublealexmarvin-design <begoublealexmarvin@gmail.com>
2026-01-30 15:20:54 +0000

    modification

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const nav = document.getElementById("navbar");
 
 toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
+
+    if (nav.classList.contains("active")) {
+        toggle.textContent = "✖";
+    } else {
+        toggle.textContent = "☰";
+    }
 });
